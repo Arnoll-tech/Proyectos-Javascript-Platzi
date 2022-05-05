6618d6fae4beafaf8db50de07391e2bc60a5f989
arnoll-tech <arnolbermudez28@gmail.com>
2022-05-05 17:38:48 -0500

    segundo metodo de los arrays

--- metodos-arrays.js
+++ metodos-arrays.js
@@ -1,3 +1,5 @@
+// metodo .filter par afiltrar los elemnetos de un array segun una caracteristica
+/*
 let articulos = [
     {nombre : "Bicicleta", costo: 3000 },
     {nombre : "Televisor", costo: 4500 },
@@ -12,3 +14,18 @@ let articulosFiltrados = articulos.filter(function(articulo){
     return articulo.costo <= 500;
 });
 console.log(articulosFiltrados);
+*/
+
+let articulos = [
+    {nombre : "Bicicleta", costo: 3000 },
+    {nombre : "Televisor", costo: 4500 },
+    {nombre : "Libro", costo: 350 },
+    {nombre : "Celular", costo: 2200 },
+    {nombre : "Laptop", costo: 3600 },
+    {nombre : "teclado", costo: 500 },
+    {nombre : "Audifonos", costo: 400 },
+];
+let nombreArticulo = articulos.map(function (articulo) {
+    return articulo.nombre
+})
+console.log(nombreArticulo);
